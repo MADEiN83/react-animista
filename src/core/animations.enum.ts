b1f4a1f3f5f7ed31addae0107625e3a0130c9ec1
Anthony DI STEFANO <ADIS@supralog.local>
2019-08-01 10:44:20 +0200

    All scale-down animations

--- src/core/animations.enum.ts
+++ src/core/animations.enum.ts
@@ -1,4 +1,5 @@
 enum AnimationTypes {
+  /* SCALE UP */
   SCALE_UP_CENTER = "scale-up-center",
   SCALE_UP_TOP = "scale-up-top",
   SCALE_UP_TR = "scale-up-tr",
@@ -13,7 +14,24 @@ enum AnimationTypes {
   SCALE_UP_HOR_RIGHT = "scale-up-hor-right",
   SCALE_UP_VER_CENTER = "scale-up-ver-center",
   SCALE_UP_VER_TOP = "scale-up-ver-top",
-  SCALE_UP_VER_BOTTOM = "scale-up-ver-bottom"
+  SCALE_UP_VER_BOTTOM = "scale-up-ver-bottom",
+
+  /* SCALE DOWN */
+  SCALE_DOWN_CENTER = "scale-down-center",
+  SCALE_DOWN_LEFT = "scale-down-left",
+  SCALE_DOWN_TR = "scale-down-tr",
+  SCALE_DOWN_BL = "scale-down-bl",
+  SCALE_DOWN_HOR_CENTER = "scale-down-hor-center",
+  SCALE_DOWN_RIGHT = "scale-down-right",
+  SCALE_DOWN_VER_BOTTOM = "scale-down-ver-bottom",
+  SCALE_DOWN_BOTTOM = "scale-down-bottom",
+  SCALE_DOWN_HOR_LEFT = "scale-down-hor-left",
+  SCALE_DOWN_TL = "scale-down-tl",
+  SCALE_DOWN_VER_CENTER = "scale-down-ver-center",
+  SCALE_DOWN_BR = "scale-down-br",
+  SCALE_DOWN_HOR_RIGHT = "scale-down-hor-right",
+  SCALE_DOWN_TOP = "scale-down-top",
+  SCALE_DOWN_VER_TOP = "scale-down-ver-top"
 }
 
 export default AnimationTypes;
